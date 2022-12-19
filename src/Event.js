4072b40e417560e1d6f03d9d1141e36efeab9f4d
jamkerr <kerr0088@gmail.com>
2022-12-19 22:38:23 +0100

    4.3: Add tests and passing functionality for showing and hiding event info

--- src/Event.js
+++ src/Event.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export function Event () {
-    return(
-        <div></div>
-    );
+export class Event extends Component {
+
+    state = { isCollapsed: true };
+
+    toggleDetails = () => {
+        this.setState((currentState) => ({
+            isCollapsed: !currentState.isCollapsed
+        }));
+    }
+
+    render() {
+        const { event } = this.props;
+        const { isCollapsed } = this.state;
+        return(
+            <div className='event'>
+                <h2 className='summary'>{event.summary}</h2>
+                <div className='location'>{event.location}</div>
+                <div className='when'>{event.start.dateTime}</div>
+
+                {!isCollapsed && (
+                    <div className='description'>{event.description}</div>
+                )}
+
+                <button
+                    className='toggle-details'
+                    onClick={() => this.toggleDetails()}
+                >{isCollapsed ? 'Show Details' : 'Hide Details'}</button>
+            </div>
+
+
+        );
+    }
 };
---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Event } from "../Event";
+
+import { mockData } from '../mock-data';
+
+
+describe('<Event /> component', () => {
+
+    let eventData, EventWrapper;
+    beforeAll(() => {
+        eventData = mockData[0];
+        EventWrapper = shallow(<Event event={eventData} />);
+    })
+
+    // Test whether default fields exist
+    test('render event fields', () => {
+        expect(EventWrapper.find('.summary')).toHaveLength(1);
+        expect(EventWrapper.find('.location')).toHaveLength(1);
+        expect(EventWrapper.find('.when')).toHaveLength(1);
+    });
+
+    // Test whether default fields are filled correctly
+    test('render event information', () => {
+        expect(EventWrapper.find('.summary').text()).toBe(eventData.summary);
+        expect(EventWrapper.find('.location').text()).toBe(eventData.location);
+        expect(EventWrapper.find('.when').text()).toBe(eventData.start.dateTime);
+    });
+
+    // Test whether user can click to reveal extra details
+    test('render event details on click', () => {
+        EventWrapper.setState({
+            isCollapsed: true
+        });
+        EventWrapper.find('.toggle-details').simulate('click');
+        expect(EventWrapper.find('.description').text()).toBe(eventData.description);
+    });
+
+    // Test whether user can click to hide extra details
+    test('hide event details on click', () => {
+        EventWrapper.setState({
+            isCollapsed: false
+        });
+        EventWrapper.find('.toggle-details').simulate('click');
+        expect(EventWrapper.find('.description')).toHaveLength(0);
+    });
+});
