87f5a993b47c0f3330ee555f778b3472c4cf1056
jamkerr <kerr0088@gmail.com>
2023-01-02 21:47:32 +0100

    4.5: Add end to end testing for city filter - Default behaviour needs better test - currently it only tests for an event, regardless of locaiton - Selecting city behaviour currently fails, WIP

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,5 +1,9 @@
 import puppeteer from 'puppeteer';
 
+// import { extractLocations } from '../api';
+import { mockData } from '../mock-data';
+
+
 describe('show/hide an event details', () => {
     
     let browser;
@@ -37,6 +41,50 @@ describe('show/hide an event details', () => {
         await page.click('.event .toggle-details');
         const eventDetails = await page.$('.event .description');
         expect(eventDetails).toBeNull();
+    });
+
+});
+
+
+describe('filter events by city', () => {
+
+    // const locations = extractLocations(mockData);
+    let browser;
+    let page;
+    beforeAll(async () => {
+        jest.setTimeout(30000);
+        browser = await puppeteer.launch(
+            // {
+            // headless: false,
+            // slowMo: 250, // slow down by 250ms
+            // ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
+            // }
+        );
+        page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+        await page.waitForSelector('.event');
+    });
+
+    afterAll(() => {
+        browser.close();
+    });
+
+    test('When user hasn\'t searched for a city, show upcoming events from all cities', async () => {
+        const eventList = await page.$('.event-list li');
+        expect(eventList).toBeDefined(); //This only tests whether at least one event is shown, not what the location is. Should be improved.
+    });
+
+    test('User should see a list of suggestions when they search for a city', async () => {
+        await page.type('.city', 'Berlin');
+        const suggestionList = await page.$('.CitySearch .suggestions');
+        expect(suggestionList).toBeDefined();
+    });
+
+    // This test currently fails
+    test('User can select a city from the suggested list', async () => {
+        await page.click('.suggestions li');
+        const eventLocation = await page.$('.event .location');
+        expect(eventLocation).stringContaining('Berlin');
     });
 
 });
