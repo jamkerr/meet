127cd0fe267a3553fd1d916f912252f36ab93be0
jamkerr <kerr0088@gmail.com>
2023-01-03 21:52:39 +0100

    4.6: Import and test Atatus

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { App } from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('7d6f65ada42c4e2c86aa83fb0ede6560').install();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -21,3 +24,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+atatus.notify(new Error('Test Atatus Setup'));
